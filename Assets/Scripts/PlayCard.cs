f521924bfb4cd9d8ffa75cef8d059a39ef7a7048
Joedv423 <Joseph.Varnes@rockets.utoledo.edu>
2015-03-04 17:26:09 -0500

    no message

---
+++ Assets/Scripts/PlayCard.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayCard : MonoBehaviour {
+
+    public GameObject placeHolder;
+    private bool mouseUp;
+	private Sprite newSprite;
+
+	void OnTriggerEnter2D(Collider2D collision)
+    {
+            transform.position = placeHolder.transform.position;
+
+            transform.localScale += new Vector3(4F + (collision.gameObject.transform.localScale.x * 0.3f), 4f + (collision.gameObject.transform.localScale.y * 0.3f), 0);
+			Destroy(gameObject);
+    }
+
+}
