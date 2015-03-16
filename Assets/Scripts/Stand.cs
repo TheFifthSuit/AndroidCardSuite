b0dc3c53e869f7edbe04ab87c3a100cdcebf8cba
Matthew Chapman <Matthew.Chapman@rockets.utoledo.edu>
2015-03-16 16:38:10 -0400

    New Changes Blackjack should completely work

---
+++ Assets/Scripts/Stand.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Stand : MonoBehaviour {
+
+
+	void OnClick()
+	{
+
+		GameObject manager = GameObject.Find("_Manager");
+		BlackjackMain bjmain = manager.GetComponent<BlackjackMain>();
+
+		bjmain.Stand();
+
+	}
+
+
+}
