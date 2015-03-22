9a81ed2ae4ca61553b79bae3ad0632a42785ebb5
Joedv423 <Joedv423@Hotmail.com>
2015-03-22 13:23:42 -0400

    no message

--- Assets/Scripts/War/DisplayPlayButton.cs
+++ Assets/Scripts/War/DisplayPlayButton.cs
@@ -4,22 +4,17 @@ using System.Collections;
 public class DisplayPlayButton : MonoBehaviour {
 
 	public GameObject playbutton;
-	private float posX;
-    private float posY;
 
 	void Start()
 	{
-		posX = transform.position.x;
-		posY = transform.position.y;
+
 	}
     
     void OnMouseDown()
     {
-        
-        //this if is added so opponents cards do not display play button when clicked
-        if (posY < 0.0f)
-        {
-            Instantiate(playbutton, new Vector3(posX, posY + transform.localScale.y, 0), Quaternion.identity);
-        }
+		GameObject manager = GameObject.Find("_Manager");
+		WarMain warmain = manager.GetComponent<WarMain>();
+
+		warmain.setPlayButton (gameObject.name);
     }
 }
