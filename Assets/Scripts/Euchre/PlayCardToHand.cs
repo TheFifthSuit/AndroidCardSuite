19871a593ca0651c16b474932621be94fbe47122
Matthew Chapman <Matthew.Chapman@rockets.utoledo.edu>
2015-04-21 21:48:30 -0400

    All that I have

---
+++ Assets/Scripts/Euchre/PlayCardToHand.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayCardToHand : MonoBehaviour {
+
+
+	void OnMouseDown()
+	{
+		if (Application.loadedLevelName == "Euchre")
+		{
+			GameObject manager = GameObject.Find("_Manager");
+			EuchreMain emain = manager.GetComponent<EuchreMain>();
+
+			if (emain.cardIsPlayed == false)
+			{
+				emain.playPlayerCardClicked(gameObject.name);
+				//print (gameObject.name);
+			}
+
+		}
+	}
+	
+
+}
